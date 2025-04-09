f7cc08209bacc6ba4bcdc7f0c80dc91af68b81dd
Ronald Garcia <ronald_amorim_garcia@DESKTOP-E53U7LF.>
2025-04-08 22:17:15 -0300

    feat: add an interactive button for each position of the board

--- src/App.js
+++ src/App.js
@@ -1,3 +1,30 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import { useState } from "react";
+
+export default function Board() {
+  return (
+  <>
+    <div className="board-row">
+      <Square/>
+      <Square/>
+      <Square/>
+    </div>
+    <div className="board-row">
+      <Square/>
+      <Square/>
+      <Square/>
+    </div>
+    <div className="board-row">
+      <Square/>
+      <Square/>
+      <Square/>
+    </div>
+  </>
+  ); 
+}
+function Square() {
+  const [value, setValue] = useState(null) // returns a object and a function to modify the object value
+  function handleClick() {
+    setValue("X") // every time this function is used, it will call the setValue function and update value const
+  }
+  return <button className="square" onClick={handleClick}>{value}</button> // when the button is clicked, the function handleClick() is called and react will re-render the elements
 }
